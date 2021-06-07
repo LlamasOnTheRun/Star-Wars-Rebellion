5cb35575fa5de96850cd241940ebcadb75ac380a
Ryan Llamas <50928563+DramaLlamaDiscover@users.noreply.github.com>
2021-06-06 23:26:14 -0500

    Quick todo to remind me

--- swrbackend/src/main/java/com/starwars/rebellion/rebellion/model/RegionsWithPlanets.java
+++ swrbackend/src/main/java/com/starwars/rebellion/rebellion/model/RegionsWithPlanets.java
@@ -14,6 +14,6 @@ import java.io.Serializable;
 public class RegionsWithPlanets implements Serializable {
     @Column
     private int region_id;
-    @Id
+    @Id/*TODO this will have to change, it is now the id. This is a One To Many relationshop. Not sure how to set up so I need to change this in future*/
     private String planet_name;
 }
